fee4b18751e1ad0756ee65f5690ebc10d1b452b0
sneurlax <sneurlax@gmail.com>
2016-12-25 15:05:38 -0600

    page title updates

--- www/info/sdc.php
+++ www/info/sdc.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>bpoch.info</title>
+    <title>bpoch.info/sdc</title>
     <link rel="shortcut icon" href="/img/favicon.png" type="image/x-icon">
 
     <link href="css/bootstrap-reboot.css" rel="stylesheet">
